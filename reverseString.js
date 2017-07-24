bc7edb0c44b58015ecd3e7f6dce886e7c56af597
Jeremiahogutu <jeremiahogutu@protonmail.com>
2017-07-24 00:13:27 -0400

    added reverse string function

--- reverseString.js
+++ reverseString.js
@@ -1,27 +1,22 @@
-# Javascript Algorithims
-
 /*
 * ----------------------------------------------------------------------------------------
-# A function that reverses a string
+*  A function that reverses a string
 * ----------------------------------------------------------------------------------------
 */
 
 function reverseString(str) {
 // split() method that takes a string and splits it into an array 
-
 	var array = str.split("");
 
 //reverse() method takes in an ARRAY reverses it. It does not take strings thus the reason we used the split method previously to make an array.
-
 	array.reverse();
 
 //join() method takes in an ARRAY an array and joins it into a string
-
 	var result = array.join("");
 
   return result;//return the string
 }
 
-//log out my results to the conosole
-
+//log out my results to the console
 console.log(reverseString("hello"));
+
