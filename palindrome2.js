93a1bbad2e2033e623a242960b7010d7e0f2ce91
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-07-26 00:20:25 -0400

    code cleanup

--- palindrome2.js
+++ palindrome2.js
@@ -14,13 +14,12 @@ function palindrome(str) {
  // g matches all characters
  
  //Here we use the toLowercase to make every character lowercase. We then use method chaining to add the replace method which searches a string for a specified value, or a regular expression, and returns a new string where the specified values are replaced.
- // 
+ 
 
 	str = str.toLowerCase().replace(/[\W_]/g, "");
 
 //Here we use the split method to return an array of substrings. We then use method chaining to add the reverse method which revereses the order of elements in array. Finally we add the join method to which joins the elements of an array into a string, and returns the string
-// 
-// 
+ 
   return str === str.split("").reverse().join("")
 
   
